7aa2f669c363dfb6748729347a57b7e82d1153a4
Mukhammadayub Siddikov <m.siddikov@gmail.com>
2017-09-12 06:28:18 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+}
+
+function addElementToBeginningOfArray(array, element){
+  return [element,...array]
+}
+
+function addElementToEndOfArray(array, element){
+  
+}
